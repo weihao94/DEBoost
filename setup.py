97d7927cd8369d2680f16a20d040caf887982aff
Khoong Wei Hao <khoongweihao@hotmail.com>
2020-05-17 15:45:59 +0800

    updated url to repo

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setuptools.setup(
     description="DEBoost: A Python Library for Weighted Distance Ensembling in Machine Learning",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/pypa/sampleproject",
+    url="https://github.com/weihao94/DEBoost",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
