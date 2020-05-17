2d2860e11808993a02aa8959b74c45b68a992209
Khoong Wei Hao <khoongweihao@hotmail.com>
2020-05-17 15:39:10 +0800

    added setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="deboost-khoongweihao", # Replace with your own username
+    version="0.10.0",
+    author="Wei Hao Khoong",
+    author_email="khoongweihao@u.nus.edu",
+    description="DEBoost: A Python Library for Weighted Distance Ensembling in Machine Learning",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pypa/sampleproject",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
