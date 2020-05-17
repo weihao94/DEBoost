bf822b3616cda0650106758f079c6178e7415473
Khoong Wei Hao <khoongweihao@hotmail.com>
2020-05-17 15:59:17 +0800

    edited setup.py to have shorter package name

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="deboost-khoongweihao", # Replace with your own username
+    name="deboost",
     version="0.10.0",
     author="Wei Hao Khoong",
     author_email="khoongweihao@u.nus.edu",
